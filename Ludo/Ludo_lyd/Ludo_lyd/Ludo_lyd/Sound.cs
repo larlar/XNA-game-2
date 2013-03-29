d1e229ff8fda3161b6abac32361517835e9a8884
Niklas Løtveit <niklas000@live.com>
2013-03-29 17:34:01 +0100

    Sound and music for game
    Feedback hadde vært bra.
    
    Slik det er nå.
    
    SoundEffect.
    -Spille av.
    -Stoppe
    -pause
    -Repeterer ikke
    
    MediaPlayer.
    -Spille av.
    -Stoppe.
    -Pause.
    -Justere volum.
    -Repeterer.

---
+++ Ludo/Ludo_lyd/Ludo_lyd/Ludo_lyd/Sound.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Ludo_lyd
+{
+    class Sound
+    {
+
+        public Sound()
+        {
+
+        }
+
+        public void playSound(KeyboardState key, SoundEffectInstance sound)
+        {
+            if (key.IsKeyDown(Keys.P))
+            {
+                sound.Play();
+            }
+        }
+
+        public void stopSound(KeyboardState key, SoundEffectInstance sound)
+        {
+            if (key.IsKeyDown(Keys.S))
+            {
+                sound.Stop();
+            }
+        }
+
+        public void pauseSound(KeyboardState key, SoundEffectInstance sound)
+        {
+            if (key.IsKeyDown(Keys.E) && sound.State == SoundState.Playing)
+            {
+                sound.Pause();
+            }
+        }
+
+    }
+}
